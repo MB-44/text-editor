0cd22fc5249efe0a0992df790ede6b1cd195221d
Menath <93506278+Lakvindubaddegama@users.noreply.github.com>
2023-11-18 21:24:05 +0530

    11/18 | Almost there

---
+++ main.py
@@ -0,0 +1,46 @@
+import PySimpleGUI as ps 
+from pathlib import Path
+
+menuLayout = [
+                ['File',['Open','Save','option','---','quit']],
+                ['Tools',['Word count']]
+             ]
+
+layout = [
+            [ps.Menu(menuLayout)],
+            [ps.Text('Untitled',key='-docName-')],
+            [ps.Multiline(
+                no_scrollbar=True,
+                size=(80,30),
+                key = "-textBox-"
+                )]
+         ]
+
+window = ps.Window("NotePad by mena",layout)
+
+while True:
+    ps.theme("TanBlue") 
+    event,values = window.read()
+    
+    if event == ps.WIN_CLOSED:
+        break
+
+    if event == "Open":
+        filePath = ps.popup_get_file('open',no_window=True)
+        if filePath:
+            file = Path(filePath)
+            window['-textBox-'].update(file.read_text())
+            window['-docName-'].update(filePath.split('/')[-1])
+
+    if event == "Save":
+        filePath = ps.popup_get_file("Save as",no_window=True,save_as=True)
+        file = Path(filePath)
+        file.write_text(values['-textBox-'])
+        window['-docName-'].update(filePath.split('/')[-1])
+
+    if event == "Word count":
+        fullText = values['-textBox-']
+        wordLen = len(fullText.replace('\n',' ').split(' '))
+        ps.popup(f"{wordLen} number of words!")
+
+window.close()
